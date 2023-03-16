a86d9d558fcf5a075bf4c90064301b430cc20e80
Rebecca Ferreira <beccixzin@gmail.com>
2023-03-16 20:30:50 -0300

    aprofundamento-express

--- src/index.ts
+++ src/index.ts
@@ -22,12 +22,24 @@ app.get('/users', (req: Request, res: Response) => {
     res.status(200).send(user)
 })
 
+app.get('/users/:id/purchases', (req: Request, res: Response) => {
+    const id: string = req.params.id
+    const result: TPurchase[] = purchase.filter(item => item.userId === id)
+    res.status(200).send(result)
+})
+
 app.get('/products', (req: Request, res: Response) => {
     res.status(200).send(product)
 })
 
 app.get('/products/search', (req: Request, res: Response) =>{
-  res.status(200).send(req.query.q as string ? product.filter(item => item.name.toLowerCase().includes((req.query.q as string).toLowerCase())) : product)
+    res.status(200).send(req.query.q as string ? product.filter(item => item.name.toLowerCase().includes((req.query.q as string).toLowerCase())) : product)
+})
+
+app.get('/products/:id', (req: Request, res: Response) => {
+    const id: string = req.params.id
+    const result: TProduct = product.find(item => item.id === id)
+    res.status(200).send(result)
 })
 
 app.post('/users', (req: Request, res: Response) => {
@@ -36,9 +48,7 @@ app.post('/users', (req: Request, res: Response) => {
         email: req.body.email as string,
         password: req.body.password as string
     }
-
     user.push(newUser)
-    console.log(user)
     res.status(201).send("Cadastro realizado com sucesso")
 })
 
@@ -51,7 +61,6 @@ app.post('/products', (req: Request, res: Response) => {
     }
 
     product.push(newProduct)
-    console.log(product)
     res.status(201).send("Produto cadastrado com sucesso")
 })
 
@@ -64,6 +73,55 @@ app.post('/purchases', (req: Request, res: Response) => {
     }
 
     purchase.push(newPurchases)
-    console.log(purchase)
     res.status(201).send("Compra realizada com sucesso")
+})
+
+app.put('/users/:id', (req: Request, res: Response) => {
+    const id: string = req.params.id
+    const newEmail: string | undefined = req.body.email
+    const newPassword: string | undefined = req.body.password
+    const userToModify: TUser = user.find(item => item.id === id)
+
+    if (userToModify) {
+        userToModify.email = newEmail || userToModify.email; 
+        userToModify.password = newPassword || userToModify.password; 
+    }
+
+    res.status(201).send("Cadastro atualizado com sucesso")
+})
+
+app.put('/products/:id', (req: Request, res: Response) => {
+    const id: string = req.params.id
+    const newName : string | undefined = req.body.name
+    const newPrice: number | undefined = req.body.price
+    const newCategory: string | undefined = req.body.category
+    const productToModify : TProduct = product.find(item => item.id === id)
+
+    if (productToModify) {
+        productToModify.name = newName || productToModify.name; 
+        productToModify.price = newPrice || productToModify.price; 
+        productToModify.category = newCategory || productToModify.category
+    }
+
+    res.status(201).send("Cadastro atualizado com sucesso")
+})
+
+app.delete('/users/:id', (req: Request, res: Response) => {
+    const id: string = req.params.id
+    const index: number = user.findIndex(item => item.id === id)
+
+    if(index >= 0) {
+        user.splice(index, 1)
+    }
+    res.status(200).send("Usuario Deletado")
+})
+
+app.delete('/products/:id', (req: Request, res: Response) => {
+    const id: string = req.params.id
+    const index = product.findIndex(item => item.id === id)
+
+    if(index >= 0) {
+        product.splice(index, 1)
+    }
+    res.status(200).send("Produto apagado com sucesso")
 })
